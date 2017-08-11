c5737842d70ce033de6a519bc2e72561d95f5216
Stela Palii <scientia.ssp@gmail.com>
2017-08-11 09:58:49 -0700

    Changed ranges for scores

--- src/convertScoreToColorAndEmoji.js
+++ src/convertScoreToColorAndEmoji.js
@@ -3,19 +3,19 @@
 export default function convertScoreToColorAndEmoji(score: mixed) {
   const currentScore = parseFloat(score);
 
-  if (currentScore < -0.4) {
+  if (currentScore < -0.3) {
     return { color: 'red', emoji: 'frustrated' };
   }
-  if (currentScore >= -0.4 && currentScore < -0.02) {
+  if (currentScore >= -0.3 && currentScore < -0.04) {
     return { color: 'yellow', emoji: 'sad' };
   }
-  if (currentScore >= -0.02 && currentScore < 0.02) {
+  if (currentScore >= -0.04 && currentScore < 0.04) {
     return { color: 'grey', emoji: 'neutral' };
   }
-  if (currentScore >= 0.02 && currentScore < 0.4) {
+  if (currentScore >= 0.04 && currentScore < 0.3) {
     return { color: 'green', emoji: 'smile' };
   }
-  if (currentScore >= 0.4) {
+  if (currentScore >= 0.3) {
     return { color: 'blue', emoji: 'happy' };
   }
   return { color: 'white', emoji: 'neutral' };
