912190013395fe4cadecac2a2e07d796c6b8fc8c
Stela Palii <scientia.ssp@gmail.com>
2017-08-21 15:18:12 -0700

    Move Reducers to their own directory

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,9 +1,77 @@
 import { combineReducers } from 'redux';
-import * from '../Actions/index.js';
+import { CONNECTED_WITH_SLACK, RECEIVED_CHANNEL_LIST, RECEIVED_MESSAGES_FOR_CHANNEL, RECEIVED_NEW_MESSAGES, RECEIVED_NEW_SCORE, SELECT_CHANNEL } from '../Actions/index.js';
 
-// CONNECTED_WITH_SLACK
-// RECEIVED_CHANNEL_LIST
-// RECEIVED_MESSAGES_FOR_CHANNEL
-// RECEIVED_NEW_MESSAGES
-// RECEIVED_NEW_SCORE
-// SELECT_CHANNEL
+export default function storeReducer(state: State, action): State {
+  let newChannelData: ChannelData;
+  let newScoreData: {[string]: ?number};
+
+  let newSelectedChannel;
+  switch (action.type) {
+    case CONNECTED_WITH_SLACK:
+      return {
+        ...state,
+        isConnectedWithSlack: true,
+      };
+
+    case SELECT_CHANNEL:
+      return {
+        ...state,
+        selectedChannel: action.channel,
+      };
+
+    case RECEIVED_CHANNEL_LIST:
+      newChannelData = { ...state.channelData };
+      action.channels.forEach((channel) => {
+        newChannelData[channel] = newChannelData[channel] || null;
+      });
+      newSelectedChannel = state.selectedChannel;
+      if (!newSelectedChannel) {
+        newSelectedChannel = action.channels[0];
+      }
+      return {
+        ...state,
+        channelData: newChannelData,
+        selectedChannel: newSelectedChannel,
+      };
+    case RECEIVED_MESSAGES_FOR_CHANNEL:
+      newChannelData = { ...state.channelData };
+      newChannelData[action.channel] = { ...newChannelData[action.channel] };
+      Object.keys(action.messages).forEach((id) => {
+        newChannelData[action.channel][id] = action.messages[id];
+      });
+      return {
+        ...state,
+        channelData: newChannelData,
+      };
+
+    case RECEIVED_NEW_SCORE:
+      newScoreData = { ...state.scoreData, ...action.scoreData };
+      return {
+        ...state,
+        isShowingScores: true,
+        scoreData: newScoreData,
+      };
+    case 'SHOW_SCORE':
+      return {
+        ...state,
+        isShowingScores: true,
+      };
+
+    case RECEIVED_NEW_MESSAGES:
+      newChannelData = { ...state.channelData };
+      Object.keys(action.messages).forEach((channelId) => {
+        newChannelData[channelId] = { ...newChannelData[channelId] };
+        Object.keys(action.messages[channelId]).forEach((messageId) => {
+          newChannelData[channelId][messageId] = action.messages[channelId][messageId];
+        });
+      });
+
+      return {
+        ...state,
+        channelData: newChannelData,
+      };
+
+    default:
+      return state;
+  }
+}
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@
 
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import storeReducer from './Reducers/index.js';
 
 export type Id = mixed;
 
@@ -21,81 +22,6 @@ export type State = {
   scoreData: {[string]: ?number},
   selectedChannel: ?string,
 };
-
-export function storeReducer(state: State, action): State {
-  let newChannelData: ChannelData;
-  let newScoreData: {[string]: ?number};
-
-  let newSelectedChannel;
-  switch (action.type) {
-    case 'CONNECTED_WITH_SLACK':
-      return {
-        ...state,
-        isConnectedWithSlack: true,
-      };
-
-    case 'SELECT_CHANNEL':
-      return {
-        ...state,
-        selectedChannel: action.channel,
-      };
-
-    case 'RECEIVED_CHANNEL_LIST':
-      newChannelData = { ...state.channelData };
-      action.channels.forEach((channel) => {
-        newChannelData[channel] = newChannelData[channel] || null;
-      });
-      newSelectedChannel = state.selectedChannel;
-      if (!newSelectedChannel) {
-        newSelectedChannel = action.channels[0];
-      }
-      return {
-        ...state,
-        channelData: newChannelData,
-        selectedChannel: newSelectedChannel,
-      };
-    case 'RECEIVED_MESSAGES_FOR_CHANNEL':
-      newChannelData = { ...state.channelData };
-      newChannelData[action.channel] = { ...newChannelData[action.channel] };
-      Object.keys(action.messages).forEach((id) => {
-        newChannelData[action.channel][id] = action.messages[id];
-      });
-      return {
-        ...state,
-        channelData: newChannelData,
-      };
-
-    case 'RECEIVED_NEW_SCORE':
-      newScoreData = { ...state.scoreData, ...action.scoreData };
-      return {
-        ...state,
-        isShowingScores: true,
-        scoreData: newScoreData,
-      };
-    case 'SHOW_SCORE':
-      return {
-        ...state,
-        isShowingScores: true,
-      };
-
-    case 'RECEIVED_NEW_MESSAGES':
-      newChannelData = { ...state.channelData };
-      Object.keys(action.messages).forEach((channelId) => {
-        newChannelData[channelId] = { ...newChannelData[channelId] };
-        Object.keys(action.messages[channelId]).forEach((messageId) => {
-          newChannelData[channelId][messageId] = action.messages[channelId][messageId];
-        });
-      });
-
-      return {
-        ...state,
-        channelData: newChannelData,
-      };
-
-    default:
-      return state;
-  }
-}
 
 /* eslint-disable */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
